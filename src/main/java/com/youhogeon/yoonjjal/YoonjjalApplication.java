acd09b15e0c4a1a94b9da271385080d6fbfb2643
YouHoGeon <me@youhogeon.com>
2022-10-07 17:15:46 +0900

    init

---
+++ src/main/java/com/youhogeon/yoonjjal/YoonjjalApplication.java
@@ -0,0 +1,13 @@
+package com.youhogeon.yoonjjal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonjjalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonjjalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youhogeon/yoonjjal/YoonjjalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youhogeon.yoonjjal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonjjalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
